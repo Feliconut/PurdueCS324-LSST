aacb7f3300f7756699eb68ac219eaf150028048e
felix_liu <feliconut@outlook.com>
2021-02-16 07:47:38 +0800

    lightcurve plot

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # PurduePHYS324-LSST
 ## SETTING UP THE ENVIRONMENT
 
+### Module Dependency
+
 ```bash
 pip install -r requirement.txt
 ```
+
+### IDE
+
+VSCode with Python Extension is optimal, because it allows separating blocks using `#%%`, making the code easy to manipulate and test.
+
+Jupyter Notebook is great for preserving outputs. The Interactive Shell in VSCode can be exported to a jupyter notebook.
--- antares_test.py
+++ antares_test.py
@@ -16,3 +16,37 @@ radius = Angle("1s")
 for locus in cone_search(center, radius):
     pass
 # %%
+# query
+query = {
+    "query": {
+        "bool": {
+            "filter": [
+                {
+                    "range": {
+                        "properties.num_mag_values": {
+                            "gte": 50,
+                            # "lte": 100,
+                        }
+                    }
+                },
+                {
+                     "term": {
+                         "tags": "nuclear_transient"
+                     }
+                }
+             ]
+        }
+    }
+}
+# %% 
+from antares_client.search import search
+sr = search(query)
+# %%
+first_result = next(sr)
+# %%
+import matplotlib.pyplot as plt
+for pb, df in first_result.lightcurve.groupby('ant_passband'):
+    plt.scatter(df['ant_mjd'],df['ant_mag'],label=pb, color=pb)
+plt.title(first_result.properties['ztf_object_id'])
+plt.legend()
+# %%
